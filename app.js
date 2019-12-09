b1234ce61c6dbc68655f5ecef8c48e39a8dc92fc
Stephanos Malikotsinas <stephanos@malikotsinas.de>
2019-12-09 10:58:22 +0100

    Update app.js

--- app.js
+++ app.js
@@ -17,8 +17,7 @@ var angle = 0;
 //TODO: dont save backgroundstars, seperate them from map? delete entities outside closest junks
 //TODO: make empty energy blink red
 //TODO: leave chemtrails when flying
-//TODO: make fuelcolor yellow
-
+//TODO: make fuelcolor orange
 var zoom = 100;
 
 var ui = {
